4d9a00d763e75aff4920da6989af82f5d4955a9b
lassepe <lasse.peters@mailbox.org>
2020-06-05 12:22:55 +0200

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,7 @@ using Test
 using TelegramMessageParser
 
 @testset "parsing" begin
-    files = "./data/messages.html"
+    files = ["./data/messages.html"]
     msgs = parse_messages(files)
-    println("Parsed $(length(msgs)) messages.")
-
-    println("""
-            Last message was:
-            $(last(msgs))
-
-            from_name: $(last(msgs).from_name)
-            timestamp: $(last(msgs).timestamp)
-            text:      $(last(msgs).text)
-            """)
+    println(msgs)
 end
