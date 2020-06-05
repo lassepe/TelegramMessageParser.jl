0a982ca24096b251224ea8f0fd8740d7ca8e4cb1
lassepe <lasse.peters@mailbox.org>
2020-06-05 09:41:43 +0200

    Update docs

--- src/TelegramMessageParser.jl
+++ src/TelegramMessageParser.jl
@@ -4,22 +4,62 @@ using Gumbo: HTMLDocument, HTMLElement, HTMLText, attrs, children, parsehtml, te
 using Cascadia: eachmatch, @sel_str
 using AbstractTrees: PreOrderDFS
 using Dates
+using DocStringExtensions
 
 export TelegramMessage, parse_messages
 
 "--------------------------------- Implementation ---------------------------------"
 
+"""
+$(TYPEDEF)
+
+A representation of a single message from a telegram chat.
+
+# Fields
+$(TYPEDFIELDS)
+"""
 struct TelegramMessage
+    "The raw HTML representation of the message which may be used to extract further
+    information (hint: use `Cascadia.jl` and/or `Gumbo.jl`)"
     raw::HTMLElement
+    "The time at which this message has been sent"
     timestamp::DateTime
+    "The author of this message."
     from_name::String
+    "The message text (does not include line breaks)."
     text::String
 end
 
+function Base.show(io::IO, m::TelegramMessage)
+    print(io,
+    """
+    timestamp: $(m.timestamp)
+    from_name: $(m.from_name)
+    text:      $(m.text)
+    raw:       [ommitted]
+    """)
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+Parse messages from multiple files or documents. `files_or_docs` may be any
+iterable with element type `String` (i.e. file names) or `Gumbo.HTMLDocument`.
+
+"""
 parse_messages(files_or_docs) = vcat(parse_messages.(files_or_docs)...)
+"""
+$(TYPEDSIGNATURES)
+
+Parse messages from file at path `filename`.
+"""
 parse_messages(filename::AbstractString) = parse_messages(parsehtml(read(filename,
                                                                           String)))
+"""
+$(TYPEDSIGNATURES)
 
+Parse messages from a given given html document.
+"""
 function parse_messages(doc::HTMLDocument)
     raw_messages = eachmatch(sel".message.default > .body", doc.root)
 
